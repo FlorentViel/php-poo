fc4705ccd885a6545bab93225717daa8a6feee72
Arnaud BODEL <contact@osw3.net>
2018-11-15 16:39:50 +0100

    -

--- TP POST/private/Controllers/ArticleController.php
+++ TP POST/private/Controllers/ArticleController.php
@@ -2,12 +2,23 @@
 
 namespace Controllers;
 
+use \Models\ArticleModel;
+
 class ArticleController extends PostController {
     const TYPE = "article";
 
     // Propriétés $articles
     // Tableau de la liste des articles
     private $articles = [];
+
+    private $model;
+
+    public function __construct()
+    {
+        $this->model = new ArticleModel;
+
+        // var_dump( $this->model->getAll() );
+    }
 
     /**
      * Boucle sur la liste des articles et les affiche
--- TP POST/private/Models/ArticleModel.php
+++ TP POST/private/Models/ArticleModel.php
@@ -2,6 +2,72 @@
 
 namespace Models;
 
-class ArticleModel {
-    
+class ArticleModel 
+{
+    // Stock l'instance de PDO
+    private $db;
+
+    public function __construct()
+    {
+        $this->getTable();
+
+        // Instance de PDO
+        try {
+            $this->db = new \PDO("mysql:dbname=wp_diw10;host=127.0.0.1", "root", "myosw3sql", [
+                \PDO::ATTR_ERRMODE => \PDO::ERRMODE_WARNING,
+                \PDO::ATTR_DEFAULT_FETCH_MODE => \PDO::FETCH_ASSOC
+            ]);
+        } catch(\Exception $e) {
+            echo 'Connexion échouée : ' . $e->getMessage();
+        }
+    }
+
+    private function getTable()
+    {
+        // Récupère le namespace de la classe (Models\ArticleModel)
+        $table = get_called_class();
+
+        // Explose la chaine sur les backslaches du namspace
+        // - Models
+        // - ArticleModel
+        $table = explode("\\", $table);
+
+        // Récupère la derniere entrée du table (ArticleModel)
+        $table = end($table);
+
+        // Remplace la chaine "Model" par rien.. (Article)
+        $table = str_replace("Model", null, $table);
+
+        // Formate la chaine "Article" en "article"
+        $table = strtolower($table);
+
+        return $table;
+    }
+
+    // List / Retrieve All
+    public function getAll()
+    {
+        $query = $this->db->query("SELECT * FROM article");
+        return $query->fetchAll();
+    }
+
+    // C - Create - Insert
+    public function create()
+    {
+    }
+
+    // R - Read /Retrieve One
+    public function getOne()
+    {
+    }
+
+    // U - Update
+    public function update()
+    {
+    }
+
+    // D - Delete
+    public function delete()
+    {
+    }
 }
